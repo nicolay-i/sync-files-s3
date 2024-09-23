79d655b7ce5548daad8d63acdc7cecce95099b52
Nicolay I <nicolay-developer@yandex.ru>
2024-09-23 20:56:03 +0700

    work of minimal sample

---
+++ Screens/ScreenSettings.ts
@@ -0,0 +1,138 @@
+import {App, PluginSettingTab, Setting, TextComponent} from "obsidian";
+import MyPlugin from "../main";
+import {createElement, Eye, EyeOff} from "lucide";
+
+export interface MyPluginSettings {
+	s3_host: string
+	s3_region: string
+	s3_access_key: string
+	s3_secret_key: string
+	s3_backet_name: string
+	password_e2e: string
+}
+
+export const DEFAULT_SETTINGS: MyPluginSettings = {
+	s3_host: '',
+	s3_region: '',
+	s3_access_key: '',
+	s3_secret_key: '',
+	s3_backet_name: '',
+	password_e2e: '',
+}
+
+export class SampleSettingTab extends PluginSettingTab {
+	plugin: MyPlugin;
+
+	constructor(app: App, plugin: MyPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('S3 host')
+			.setDesc('Example: s3.timeweb.cloud')
+			.addText(text => text
+				.setValue(this.plugin.settings.s3_host)
+				.onChange(async (value) => {
+					this.plugin.settings.s3_host = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('S3 region')
+			.setDesc('Example: en-US')
+			.addText(text => text
+				.setValue(this.plugin.settings.s3_region)
+				.onChange(async (value) => {
+					this.plugin.settings.s3_region = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('S3 access key')
+			.setDesc('Example: RYDN9J1RC8.....BOM1')
+			.addText(text => {
+				wrapTextWithPasswordHide(text);
+
+				return text
+					.setValue(this.plugin.settings.s3_access_key)
+					.onChange(async (value) => {
+						this.plugin.settings.s3_access_key = value;
+						await this.plugin.saveSettings();
+					});
+			});
+
+		new Setting(containerEl)
+			.setName('S3 secret key')
+			.setDesc('Example: J0F24GQa.....wqgJZsm9sEd')
+			.addText(text => {
+				wrapTextWithPasswordHide(text);
+
+				return text
+					.setValue(this.plugin.settings.s3_secret_key)
+					.onChange(async (value) => {
+						this.plugin.settings.s3_secret_key = value;
+						await this.plugin.saveSettings();
+					});
+			});
+
+		new Setting(containerEl)
+			.setName('S3 backet name')
+			.setDesc('Example: 2266ee23-sample-name')
+			.addText(text => text
+				.setValue(this.plugin.settings.s3_backet_name)
+				.onChange(async (value) => {
+					this.plugin.settings.s3_backet_name = value;
+					await this.plugin.saveSettings();
+				}));
+
+		if (this.plugin.settings.s3_backet_name != '')
+			new Setting(containerEl)
+				.setName('S3 backet name')
+				.setDesc('Example: 2266ee23-sample-name-...')
+				.addText(text => text
+					.setValue(this.plugin.settings.s3_backet_name)
+					.onChange(async (value) => {
+						this.plugin.settings.s3_backet_name = value;
+						await this.plugin.saveSettings();
+					}));
+
+
+
+	}
+}
+
+export const wrapTextWithPasswordHide = (text: TextComponent) => {
+	const { eye, eyeOff } = getEyesElements();
+	const hider = text.inputEl.insertAdjacentElement("afterend", createSpan())!;
+	text.inputEl.style.paddingRight = '25px'
+	text.inputEl.style.width = '165px'
+	// the init type of hider is "hidden" === eyeOff === password
+	hider.innerHTML = eyeOff;
+	hider.setAttr('style', 'position: absolute; margin-right: 5px;')
+	hider.addEventListener("click", (e) => {
+		const isText = text.inputEl.getAttribute("type") === "text";
+		hider.innerHTML = isText ? eyeOff : eye;
+		text.inputEl.setAttribute("type", isText ? "password" : "text");
+		text.inputEl.focus();
+	});
+
+	// the init type of text el is password
+	text.inputEl.setAttribute("type", "password");
+	return text;
+};
+
+const getEyesElements = () => {
+	const eyeEl = createElement(Eye);
+	const eyeOffEl = createElement(EyeOff);
+	console.log({eyeEl: eyeEl.outerHTML})
+	return {
+		eye: eyeEl.outerHTML.replace('<svg', '<svg style="width: 15px; height: 15px;"'),
+		eyeOff: eyeOffEl.outerHTML.replace('<svg', '<svg style="width: 15px; height: 15px;"'),
+	};
+};
--- main.ts
+++ main.ts
@@ -1,14 +1,7 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./Screens/ScreenSettings";
 
 // Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -71,11 +64,33 @@ export default class MyPlugin extends Plugin {
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+			// console.log('click', evt);
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+
+		this.app.vault.on('create', function (event) {
+
+		})
+
+		this.app.vault.on('rename', function (event) {
+
+		})
+
+		this.app.vault.on('modify', function (event) {
+			// console.log({event})
+		})
+
+		this.app.metadataCache.on('deleted', function (event) {
+
+		})
+
+		this.app.metadataCache.on('changed', function (event) {
+			// console.log({event})
+
+			// new Notice("Your text", 1500)
+		})
 	}
 
 	onunload() {
@@ -107,28 +122,3 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
