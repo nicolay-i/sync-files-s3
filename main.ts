0b610cc37f37d9b0925c8bfd8786c60b72530cc7
Nicolay <admin@example.com>
2024-12-15 13:02:58 +0000

    aws-version/v1 into dev

--- main.ts
+++ main.ts
@@ -1,14 +1,21 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./Screens/ScreenSettings";
+import {
+	addFileToCreated,
+	addFileToRemove, addFileToRename,
+	addFileToUpdate, downloadRemoteConfig, isInit,
+	reCalcLocalConfigFile, runDeleteFiles,
+	runDownloadFiles, runMoveFiles,
+	runUploadFiles,
+	syncFile
+} from "./syncFile";
+import {minioConfig} from "./utils/s3_client/config";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+export const PLUGIN_ID = "sync-files-s3";
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export let statusBarItemEl: HTMLElement
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -17,16 +24,25 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		const ribbonIconEl = this.addRibbonIcon('folder-sync', 'Sync',  async (evt: MouseEvent) => {
+			new Notice('Running sync');
+
+			await reCalcLocalConfigFile();
+			await runMoveFiles();
+			await runUploadFiles();
+			await runDownloadFiles();
+			await runDeleteFiles();
+			await downloadRemoteConfig()
+
+			new Notice('Sync complete');
+
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl = this.addStatusBarItem();
+		statusBarItemEl.setText('Files not synced');
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
@@ -71,11 +87,44 @@ export default class MyPlugin extends Plugin {
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+			// console.log('click', evt);
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+
+		this.app.vault.on('create', function (event) {
+			if (isInit) {
+				console.log(`Event: created ${event.path}`)
+				addFileToCreated(event.path)
+			}
+		})
+
+		this.app.vault.on('rename', function (event, oldPathWithName) {
+			console.log(`Event: renamed ${event.path}`)
+			addFileToRename(event.path, oldPathWithName);
+		})
+
+		this.app.vault.on('modify', function (event) {
+			console.log(`Event: modify ${event.path}`)
+			addFileToUpdate(event.path)
+		})
+
+		this.app.metadataCache.on('deleted', function (event) {
+			console.log(`Event: deleted ${event.path}`)
+			addFileToRemove(event.path)
+		})
+
+		this.app.metadataCache.on('changed', function (event) {
+			console.log(`Event: changed ${event.path}`)
+			addFileToUpdate(event.path)
+		})
+
+		await this.loadData()
+		syncFile.init(this.app).then(res => {
+			console.log(`End init syncFile`)
+		})
+
 	}
 
 	onunload() {
@@ -84,6 +133,14 @@ export default class MyPlugin extends Plugin {
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+
+		minioConfig.accessKey = this.settings.s3_access_key;
+		minioConfig.secretKey = this.settings.s3_secret_key;
+		minioConfig.endpoint = this.settings.s3_host;
+		minioConfig.region = this.settings.s3_region;
+		minioConfig.bucketName = this.settings.s3_backet_name;
+		minioConfig.password_e2e = this.settings.password_e2e;
+
 	}
 
 	async saveSettings() {
@@ -107,28 +164,3 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
